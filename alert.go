e159f498c852ae87a88958f6d00584e5be144235
Zachary Seguin <zachary@zacharyseguin.ca>
2018-03-17 15:19:18 -0400

    Remove utility functions that will go in go-cap-process instead

--- alert.go
+++ alert.go
@@ -5,10 +5,6 @@ import (
 	"encoding/hex"
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 type Alert struct {
@@ -34,61 +30,4 @@ func (alert *Alert) Id() string {
 	hash := sha1.New()
 	hash.Write([]byte(fmt.Sprintf("%s,%s,%s", alert.Sender, alert.Sent.FormatCAP(), alert.Identifier)))
 	return hex.EncodeToString(hash.Sum(nil))
-}
-
-type VisitFunc func(alert *Alert) error
-
-func ParseAlert(path string) (*Alert, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	contents, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	var alert Alert
-	err = xml.Unmarshal(contents, &alert)
-	if err != nil {
-		return nil, err
-	}
-
-	return &alert, nil
-}
-
-func WalkAlerts(dir string, visitFunc VisitFunc) error {
-	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
-		// Don't process folders
-		if err != nil || f.IsDir() {
-			return err
-		}
-
-		// Only handle xml files
-		if !strings.HasSuffix(f.Name(), ".xml") {
-			return nil
-		}
-
-		alert, err := ParseAlert(path)
-		if err != nil {
-			return err
-		}
-
-		return visitFunc(alert)
-	})
-
-	return err
-}
-
-func ParseAlerts(dir string) ([]*Alert, error) {
-	alerts := make([]*Alert, 0)
-
-	err := WalkAlerts(dir, func(alert *Alert) error {
-		alerts = append(alerts, alert)
-		return nil
-	})
-
-	return alerts, err
 }
